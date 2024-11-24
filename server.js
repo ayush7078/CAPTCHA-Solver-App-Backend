0c1046b3955c59c76b0921699a4ee7926df275a2
ayush7078 <ayushporwal7078@gmail.com>
2024-11-24 05:35:18 +0530

    Create an Swagger Doc also to show the all available apis

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { connectDB } = require('./config/db'); // Import connectDB from db.js
+const { connectDB } = require('./config/db');
 const captchaRoutes = require('./routes/captchaRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json'); // Path to swagger.json file
 
 dotenv.config();
-
-// Call the connectDB function to initiate the connection
 connectDB();
 
-// Create Express app
 const app = express();
 app.use(express.json());
-app.use(cors()); // Enable CORS
+app.use(cors());
+
+// Swagger UI route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Use routes
 app.use('/api', captchaRoutes);
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`API docs available at http://localhost:${PORT}/api-docs`);
 });
